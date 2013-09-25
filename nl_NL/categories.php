0a5ff990df9a39762e3468a4d32738c9f69bd5d3
marciplan <ik@marcianoplanque.nl>
2013-09-25 13:12:49 +0200

    Update categories.php
    The old nl_NL didn't work properly anymore so I translated the en_GB to Dutch from scratch. (The only words not translated are 'unique keys', 'slug', 'extend' and 'snippet'. This is to avoid confusion when a problem occurs and a user wants to google the problem.)

--- nl_NL/categories.php
+++ nl_NL/categories.php
@@ -2,11 +2,27 @@
 
 return array(
 
-  'missing_title' => 'Vul een titel in',
-	'category_failed_delete' => 'Er moet minimaal &egrave;&egrave;n categorie aanwezig zijn',
+	'category' => 'Categorie',
+	'categories' => 'Categori&euml;en',
 
-	'category_success_created' => 'De categorie is toegevoegd.',
-	'category_success_updated' => 'De categorie is bijgewerkt.',
-	'category_success_deleted' => 'De categorie is verwijderd.'
+	'create_category' => 'Maak een nieuwe categorie',
+	'edit_category' => '&ldquo;%s&rdquo; is in bewerking.',
+
+	// form fields
+	'title' => 'Titel',
+	'title_explain' => 'De titel van je categorie.',
+	'title_missing' => 'Geef de categorie een titel.',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'De slug voor jouw categorie.',
+
+	'description' => 'Omschrijving',
+	'description_explain' => 'Waar gaat je categorie over?',
+
+	// messages
+	'created' => 'Je nieuwe categorie is aangemaakt.',
+	'updated' => 'Je categorie is ge&uuml;pdated.',
+	'deleted' => 'Je categorie is verwijderd.',
+	'delete_error' => 'Je moet minstens &eacute;&eacute;n categorie hebben.',
 
 );
